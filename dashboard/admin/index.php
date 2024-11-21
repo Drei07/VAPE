0584cf45fab0e2e464b0cec2c9e4ed04969b7e2b
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2024-11-21 23:46:02 +0800

    MAIN

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -58,65 +58,6 @@ include_once 'header.php';
 			</div>
 
 			</div>
-			<ul class="dashboard_data">
-				<div class="gauge_dashboard">
-					<div class="status">
-						<div class="card arduino">
-							<h1>DEVICE STATUS</h1>
-							<div class="sensor-data">
-								<span id="wifi_status">Loading...</span>
-							</div>
-						</div>
-					</div>
-					<div class="status">
-						<div class="card arduino">
-							<h1>WATER SENSOR STATUS</h1>
-							<div class="sensor-data">
-								<span id="waterStatus">Loading...</span>
-							</div>
-						</div>
-						<div class="card arduino">
-							<h1>PUMP SENSOR STATUS</h1>
-							<div class="sensor-data">
-								<span id="pumpStatus">Loading...</span>
-							</div>
-						</div>
-					</div>
-					<div class="status">
-						<div class="card arduino">
-							<h1>VALVE SENSOR 1 STATUS</h1>
-							<div class="sensor-data">
-								<span id="valve1Status">Loading...</span>
-							</div>
-						</div>
-						<div class="card arduino">
-							<h1>VALVE SENSOR 2 STATUS</h1>
-							<div class="sensor-data">
-								<span id="valve2Status">Loading...</span>
-							</div>
-						</div>
-					</div>
-
-					<div class="gauge">
-						<div class="card gauge_card">
-							<p class="card-title">SOIL SENSOR 1</p>
-							<div id="SoilSensor1"></div>
-						</div>
-						<div class="card gauge_card">
-							<p class="card-title">SOIL SENSOR 2</p>
-							<div id="SoilSensor2"></div>
-						</div>
-						<div class="card gauge_card">
-							<p class="card-title">HUMIDITY %</p>
-							<div id="humidity"></div>
-						</div>
-						<div class="card gauge_card">
-							<p class="card-title">TEMPERATURE °C</p>
-							<div id="temperature"></div>
-						</div>
-					</div>
-				</div>
-			</ul>
 		</main>
 		<!-- MAIN -->
 	</section>
